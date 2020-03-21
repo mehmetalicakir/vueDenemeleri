2c2ab5a6b7bdca54286c2b91a64e137afc269a6e
Mehmet Ali ÇAKIR <32022785+mehmetalicakir@users.noreply.github.com>
2020-03-21 18:36:26 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# vueDenemeleri
+# example2
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Lints and fixes files
+```
+yarn lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <ApolloExample msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import ApolloExample from './components/ApolloExample.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ApolloExample
+  }
+}
+
+const { ApolloServer } = require("apollo-server");
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  engine: {
+    apiKey: "service:denemeden:hMcrjocock_pF3IAvJjpJA",
+  }
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${ url }`);
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
